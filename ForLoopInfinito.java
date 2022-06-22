e37b14e76f19d780391cf4886a44eb16476c333f
NicolasRamires <nicolas96_ramires@hotmail.com>
2022-06-22 19:11:06 -0300

    Create ForLoopInfinito.java

---
+++ ForLoopInfinito.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class ForLoopInfinito {
+     public static void main(String[] args){
+
+         //For para laço de repetição infinita
+         for(;;){
+             System.out.println("Laço Infinito");
+         }
+
+     }
+}
